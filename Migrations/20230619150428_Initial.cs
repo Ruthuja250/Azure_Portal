0402d6a27414bb461bf469eeaa2b03c0dbb693c5
Ruthuja250 <rutujareddy250@gmail.com>
2023-06-19 21:21:39 +0530

    Application model completed

---
+++ Migrations/20230619150428_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Azure_Portal.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    C_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    C_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Application_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Serverinfo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Portinfo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.C_Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Applications");
+        }
+    }
+}
